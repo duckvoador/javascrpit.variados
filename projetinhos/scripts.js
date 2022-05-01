0f8dad4e88c3853fafbb1bec48c4513a61341ee7
Arthur <arthurguelle@gmail.com>
2022-04-30 23:25:04 -0300

    commit de 1 leva

---
+++ projetinhos/scripts.js
@@ -0,0 +1,61 @@
+	class 	ContaBancaria {
+		constructor(agencia, numero, tipo){
+			this.agencia = agencia;
+			this.numero = numero;
+			this._saldo = 0;
+			this.tipo = tipo;
+
+		}
+		get saldo() {
+			return this._saldo;
+		}
+		set saldo(valor){
+			this._saldo = valor;
+		
+		}
+		sacar(valor){
+			if (valor > this._saldo){
+				return "Operação Negada!";
+			}
+			this._saldo = this._saldo - valor;
+
+			return this._saldo;
+		}
+
+		depositar(valor) {
+			this._saldo = this._saldo + valor;
+			return this._saldo;	
+			}
+	}
+	class ContaCorrente extends ContaBancaria{
+		constructor(agencia, numero){
+			super(agencia, numero);
+			this.tipo = 'corrente';
+			this._cartaoCredito = cartaoCredito;
+		}
+
+		get cartaoCredito (){
+			return this._cartaoCredito;
+		}
+		set cartaoCredito(valor){
+			this._cartaoCredito = valor;
+		}
+	}
+	class ContaPoupança extends ContaBancaria {
+		constructor(agencia, numero){
+			super(agencia, numero);
+			this.tipo = 'poupança';
+		}
+	}	
+	class ContaUniversitaria extends ContaBancaria {
+		constructor(agencia, numero){
+			super(agencia, numero);
+			this.tipo = 'universitaria';
+		}
+		sacar(valor){
+			if (valor > 500){
+				return "Operação Negada!";
+			}
+			this._saldo = this._saldo - valor;
+		}
+	}	
